81acff9b2665c1f57e8ec702450057bff34ffd20
gaurav parsar <98256253+INDIANgaurav@users.noreply.github.com>
2024-05-09 17:39:02 +0530

    Update mailSender.js

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -3,8 +3,10 @@ const nodemailer = require("nodemailer")
 const mailSender = async (email, title, body) => {
   try {
     let transporter = nodemailer.createTransport({
-     host: process.env.MAIL_HOST,
-            
+     service:"gmail",
+      host: process.env.MAIL_HOST,
+      port:465,
+      secure:false ,
             auth:{
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
